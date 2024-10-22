c7ef7ebbbacd7c893d053a6aa484fb25fde562d7
neikey <neikey@qq.com>
2024-10-22 21:38:05 +0800

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-hugo server --theme=hyde -b 'http://10.0.3.155' --buildDrafts  --bind='10.0.3.155'
+hugo server --theme=hyde -b 'http://hugo.mi/' --appendPort=false --buildDrafts  --bind='10.0.3.155'
